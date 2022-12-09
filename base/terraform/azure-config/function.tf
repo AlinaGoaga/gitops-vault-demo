0f2f095d5660beb8db1d3a0e0237de8c7eec1121
opudrovs <opudrovs@gmail.com>
2022-12-09 18:08:26 +0100

    Add changes.

--- base/terraform/azure-config/function.tf
+++ base/terraform/azure-config/function.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "example" {
   name     = "azure-functions-test-rg"
   location = "West Europe"
